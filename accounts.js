f971fa5f7e150af5082014d14c0ea99c6955c4df
lnabcdefgh2 <157073130+lnabcdefgh2@users.noreply.github.com>
2024-02-23 08:16:39 +0800

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,16 +1,16 @@
 module.exports = [{
-  userName: process.env.TY_USER_NAME || 'userName',
-  password: process.env.TY_PASSWORD || 'password',
-}, {
+  userName: process.env.TY_USER_NAME || '17753115987',
+  password: process.env.TY_PASSWORD || 'Nc~!110120',
+}， {
   userName: process.env.TY_USER_NAME1,
   password: process.env.TY_PASSWORD1,
-}, {
+}， {
   userName: process.env.TY_USER_NAME2,
   password: process.env.TY_PASSWORD2,
-}, {
+}， {
   userName: process.env.TY_USER_NAME3,
   password: process.env.TY_PASSWORD3,
-}, {
+}， {
   userName: process.env.TY_USER_NAME4,
   password: process.env.TY_PASSWORD4,
 }];
